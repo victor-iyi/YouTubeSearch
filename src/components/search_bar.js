d8861820d3b8d92f84b77d7722885b831f543d19
victor-iyiola <javafolabi@gmail.com>
2017-07-01 02:37:49 +0100

    Class Components and states

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const SearchBar = () => {
-	return <input />
+export default class SearchBar extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = { term: '' }
+	}
+
+	render() {
+		return ( 
+			<div>
+				<input 
+					value={this.state.term}
+					onChange={event => this.setState({ term: event.target.value })} 
+				/>
+				<p>Value of the input: {this.state.term}</p>
+			</div>
+		)
+	}
+
 }
-
-export default SearchBar
