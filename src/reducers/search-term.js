5e32564b85f5bbbf99900b6ae4f42fdf34ec6e33
victor-iyiola <javafolabi@gmail.com>
2017-08-16 21:11:52 +0100

    Adding missing semi-colons to project files.

--- src/reducers/search-term.js
+++ src/reducers/search-term.js
@@ -1,8 +1,12 @@
-export default (state='artificial intelligence', action) => {
+import { SEARCH_CHANGED } from '../actions/actions';
+
+const INITIAL_STATE = 'artificial intelligence';
+
+export default (state=INITIAL_STATE, action) => {
 	switch(action.type) {
-		case 'SEARCH_CHANGED':
-			return action.payload
+		case SEARCH_CHANGED:
+			return action.payload;
 		default:
-			return state
+			return state;
 	}
-}
+};
